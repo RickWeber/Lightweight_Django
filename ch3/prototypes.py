6c512e2e5b7580cc0035cda6b5fe7be62e7b2535
RickWeber <863114+RickWeber@users.noreply.github.com>
2022-03-24 14:19:14 -0400

    get part way through chapter 3

--- ch3/prototypes.py
+++ ch3/prototypes.py
@@ -1,5 +1,8 @@
+import os
 import sys
 from django.conf import settings
+
+BASE_DIR = os.path.dirname(__file__)
 
 settings.configure(
     DEBUG=True,
@@ -11,7 +14,15 @@ settings.configure(
         #'django.contrib.webdesign', # depricated
         'sitebuilder',
     ),
-    STATIC_URL='/static/'
+    TEMPLATES=(
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [],
+            'APP_DIRS': True,
+        },
+    ),
+    STATIC_URL='/static/',
+    SITE_PAGES_DIRECTORY=os.path.join(BASE_DIR, 'pages'),
 )
 
 if __name__=="__main__":
--- ch3/sitebuilder/urls.py
+++ ch3/sitebuilder/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = ()
+from django.urls import re_path
+
+from .views import page
+
+
+urlpatterns = (
+    re_path(r'^(?P<slug>[\w./-]+)/$', page, name='page'),
+    re_path(r'^$', page, name='homepage'),
+)
